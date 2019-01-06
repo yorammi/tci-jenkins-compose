d5b5e0ce739bbd1fa890730472a71d7b2b80bc02
yorammi <yorammi@tikalk.com>
2019-01-06 06:56:36 +0200

    Better upgrade

--- tci.sh
+++ tci.sh
@@ -31,13 +31,13 @@ if [[ "$action" == "upgrade" ]]; then
     echo -e "[Hash]\t\t${BLUE}${hash}${NC}" >> info/version/version.txt
 
     diff1=`diff -q temp/templates/tci-master templates/tci-master | wc -l | xargs`
-        diff2=`diff -q temp/customization/tci-master templates/tci-master | wc -l | xargs`
-    if [[ "diff1" != "0" && "diff2" != "0" ]]; then
-        echo -e "*** ${BG_RED}NOTE:${NC} You ${BG_RED}MUST${NC} run a tci-master customization merge with new templates ***"
+    diff2=`diff -q temp/customization/tci-master templates/tci-master | wc -l | xargs`
+    if [[ "$diff1" != "0" && "$diff2" != "0" ]]; then
+        echo -e "\n*** ${BG_RED}NOTE:${NC} You ${BG_RED}MUST${NC} run a tci-master customization merge with new templates ***"
         diff -q temp/templates/tci-master templates/tci-master
     fi
 
-    echo -e "\n*** ${BG_RED}NOTE:${NC} You need to run again with '${BG_RED}init${NC}' action ***"
+    echo -e "\n*** ${BG_RED}NOTE:${NC} You need to run again with '${BG_RED}init${NC}' action ***\n"
     exit 0
 fi
 
