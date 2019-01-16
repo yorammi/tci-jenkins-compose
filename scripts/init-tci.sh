b4350d2b4e6510dd790d5ee09d092d3a15e31630
yorammi <yorammi@tikalk.com>
2019-01-16 13:04:39 +0200

    fix init script

--- scripts/init-tci.sh
+++ scripts/init-tci.sh
@@ -35,19 +35,33 @@ function inputVariable {
     export $VARIABLE_NAME=${VALUE}
 }
 
+function inputTextVariable {
+
+    TITLE=$1
+    VARIABLE_NAME=$2
+    VALUE=${!VARIABLE_NAME}
+    echo -e -n "${TITLE}\n\t[${BLUE}${VALUE}${NC}]? "
+    read -r
+    if [[ "$REPLY" != "" ]]; then
+        VALUE="$REPLY"
+    fi
+    echo "export $VARIABLE_NAME='${VALUE}'" >> temp/tci.config
+    export $VARIABLE_NAME='${VALUE}'
+}
+
 initTciScript
 initTciConfig
 
 inputVariable "tci-master image" TCI_MASTER_VERSION
 inputVariable "tci-library branch" TCI_LIBRARY_BRANCH
 inputVariable "tci-pipelines branch" TCI_PIPELINES_BRANCH
-inputVariable "TCI banner title" TCI_MASTER_TITLE_TEXT
+inputTextVariable "TCI banner title" TCI_MASTER_TITLE_TEXT
 inputVariable "TCI banner title color" TCI_MASTER_TITLE_COLOR
 inputVariable "TCI banner background color" TCI_MASTER_BANNER_COLOR
 inputVariable "Jenkins server HTTP port" JENKINS_HTTP_PORT_FOR_SLAVES
 inputVariable "Jenkins JNLP port for slaves" JENKINS_SLAVE_AGENT_PORT
 inputVariable "Number of exeuters on master" JENKINS_ENV_EXECUTERS
-inputVariable "tci in debug mode" TCI_DEBUG_MODE
+inputTextVariable "tci in debug mode" TCI_DEBUG_MODE
 inputVariable "TCI host IP address (set to * for automatic IP calculation)" TCI_HOST_IP
 
 cp temp/tci.config tci.config
