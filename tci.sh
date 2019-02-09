9dab807d54cbc8ccf66c015962ce027eabeffa9b
yorammi <yorammi@tikalk.com>
2019-02-09 14:50:06 +0200

    add timeout for curl commands

--- tci.sh
+++ tci.sh
@@ -149,15 +149,15 @@ function startTciServer {
 }
 
 function showTciServerStatus {
-    status=`curl -s -I http://localhost:$JENKINS_HTTP_PORT_FOR_SLAVES | grep "403" | wc -l | xargs`
+    status=`curl -s -I -m 5 http://localhost:$JENKINS_HTTP_PORT_FOR_SLAVES | grep "403" | wc -l | xargs`
     if [[ "$status" == "1" ]]; then
         echo -e "\n${BLUE}[TCI status] ${BG_GREEN}tci-server is up and running${NC}\n"
     else
-        status=`curl -s -I http://localhost:$JENKINS_HTTP_PORT_FOR_SLAVES | grep "401" | wc -l | xargs`
+        status=`curl -s -I -m 5 http://localhost:$JENKINS_HTTP_PORT_FOR_SLAVES | grep "401" | wc -l | xargs`
         if [[ "$status" == "1" ]]; then
             echo -e "\n${BLUE}[TCI status] ${BG_GREEN}tci-server is up and running${NC}\n"
         else
-            status=`curl -s -I http://localhost:$JENKINS_HTTP_PORT_FOR_SLAVES | grep "503" | wc -l | xargs`
+            status=`curl -s -I -m 5 http://localhost:$JENKINS_HTTP_PORT_FOR_SLAVES | grep "503" | wc -l | xargs`
             if [[ "$status" == "1" ]]; then
                 echo -e "\n${BLUE}[TCI status] ${BG_RED}tci-server is starting${NC}\n"
             else
